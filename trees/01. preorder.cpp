6cfbe1932eacf793acc63a13bef0550ee150f446
Manthan077 <manthan10041004@gmail.com>
2026-03-21 22:08:47 +0530

    feat: implement preorder traversal (iterative)

--- trees/01. preorder.cpp
+++ trees/01. preorder.cpp
@@ -9,6 +9,8 @@ struct TreeNode {
     TreeNode(int x) : val(x),left(nullptr),right(nullptr) {}
 };
 
+// Recursive approach
+
 void preorder(TreeNode* root,vector<int>& res){
     if(root==nullptr) return;
 
@@ -20,5 +22,26 @@ void preorder(TreeNode* root,vector<int>& res){
 vector<int> preorderTraversal(TreeNode* root) {
     vector<int> res;
     preorder(root,res);
+    return res;
+}
+
+
+// Iterative approach
+
+vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> res;
+    if (root == nullptr) return res;
+
+    stack<TreeNode*> st;
+    st.push(root);
+
+    while (!st.empty()) {
+        root = st.top();
+        st.pop();
+        res.push_back(root->val);
+
+        if (root->right != nullptr) st.push(root->right);
+        if (root->left != nullptr) st.push(root->left);
+    }
     return res;
 }
