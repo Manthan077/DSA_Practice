a6d9def53166213bf42e3bf10383414b7dae7f98
Manthan077 <manthan10041004@gmail.com>
2026-03-22 22:24:12 +0530

    feat: implement inorder traversal (iterative)

--- trees/02. inorder.cpp
+++ trees/02. inorder.cpp
@@ -9,6 +9,8 @@ struct TreeNode {
     TreeNode(int x) : val(x),left(nullptr),right(nullptr) {}
 };
 
+// Recursive approach
+
 void inorder(TreeNode* root,vector<int>& res){
     if(root==nullptr) return;
 
@@ -20,5 +22,26 @@ void inorder(TreeNode* root,vector<int>& res){
 vector<int> inorderTraversal(TreeNode* root) {
     vector<int> res;
     inorder(root,res);
+    return res;
+}
+
+
+// Iterative approach
+
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> res;
+    stack<TreeNode*> st;
+    TreeNode* curr = root;
+
+    while (curr != nullptr || !st.empty()) {
+        while (curr != nullptr) {
+            st.push(curr);
+            curr = curr->left;
+        }
+        curr = st.top();
+        st.pop();
+        res.push_back(curr->val);
+        curr = curr->right;
+    }
     return res;
 }
