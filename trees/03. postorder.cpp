ad92594352fd0b5602af847cafa3ccf6da5fe3b8
Manthan077 <manthan10041004@gmail.com>
2026-03-22 22:24:24 +0530

    feat: implement postorder traversal (iterative)

--- trees/03. postorder.cpp
+++ trees/03. postorder.cpp
@@ -9,6 +9,8 @@ struct TreeNode {
     TreeNode(int x) : val(x),left(nullptr),right(nullptr) {}
 };
 
+// Recursive approach
+
 void postorder(TreeNode* root,vector<int>& res){
     if(root==nullptr) return;
 
@@ -20,5 +22,27 @@ void postorder(TreeNode* root,vector<int>& res){
 vector<int> postorderTraversal(TreeNode* root) {
     vector<int> res;
     postorder(root,res);
+    return res;
+}
+
+
+// Iterative approach
+
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> res;
+    if (root == nullptr) return res;
+
+    stack<TreeNode*> st;
+    st.push(root);
+
+    while (!st.empty()) {
+        root = st.top();
+        st.pop();
+        res.push_back(root->val);
+
+        if (root->left != nullptr) st.push(root->left);
+        if (root->right != nullptr) st.push(root->right);
+    }
+    reverse(res.begin(), res.end());
     return res;
 }
