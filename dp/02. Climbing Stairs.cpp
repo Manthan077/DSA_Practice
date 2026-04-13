37e2aa92d1a646fcc28bfd85b59726e64b7b154e
Manthan077 <manthan10041004@gmail.com>
2026-04-13 21:09:32 +0530

    feat: implement climbing stairs

---
+++ dp/02. Climbing Stairs.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int climbStairs(int n){
+    if(n<=1) return 1;
+
+    int prev2 = 1; 
+    int prev1 = 1; 
+    int curr;
+
+    for(int i=2;i<=n;i++){
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<climbStairs(n);
+}
